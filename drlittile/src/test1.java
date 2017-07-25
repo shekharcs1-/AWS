7d898061b5808a0a211aed21aa231dd93b2d1bbf
shekhar <shekharcs10@gmail.com>
2017-07-26 01:10:22 +0530

    test

---
+++ drlittile/src/test1.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class test1 {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void test1() {
+		//fail("Not yet implemented");
+	}
+	@Test
+	public void test2() {
+		//fail("Not yet implemented");
+	}
+
+
+}
